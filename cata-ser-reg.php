d65d3feccb40ba8591c25af7c563b8d1a5b763f9
Mario Ernesto Basurto Medrano <mario@d80.mx>
2018-11-16 16:11:46 -0600

    no message

--- cata-ser-reg.php
+++ cata-ser-reg.php
@@ -122,7 +122,53 @@ var dPrecio = document.getElementById("dPrecio");
 		 		<div class="col-lg-12" style="max-height:150px;">
 								<h2 class="title-1 m-b-25"> </h2>
                                 <div class="card col-lg-12">
-                                    
+                                    <div style="text-align:right;">
+                                    <input type='search' id='txt_searchall' placeholder='Búsqueda rápida...'> 
+						
+						<!-- Script -->
+        <script type='text/javascript'>
+            $(document).ready(function(){
+
+                // Search all columns
+                $('#txt_searchall').keyup(function(){
+                    // Search Text
+                    var search = $(this).val();
+
+                    // Hide all table tbody rows
+                    $('table tbody tr').hide();
+
+                    // Searching text in columns and show match row
+                    $('table tbody tr td:contains("'+search+'")').each(function(){
+                        $(this).closest('tr').show();
+                    });
+                    
+                });
+
+                // Search on name column only
+                $('#txt_name').keyup(function(){
+                    // Search Text
+                    var search = $(this).val();
+
+                    // Hide all table tbody rows
+                    $('table tbody tr').hide();
+
+                    // Searching text in columns and show match row
+                    $('table tbody tr td:nth-child(2):contains("'+search+'")').each(function(){
+                        $(this).closest('tr').show();
+                    });
+                    
+                });
+               
+            });
+
+            // Case-insensitive searching (Note - remove the below script for Case sensitive search )
+            $.expr[":"].contains = $.expr.createPseudo(function(arg) {
+                return function( elem ) {
+                    return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+                };
+            });
+        </script>
+                                    </div>
                                     <div class="card-body card-block">
                                         <!--campos-->
                                         
