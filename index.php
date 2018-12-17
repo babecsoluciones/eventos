413e26f44728dbe63b4d5f4dcce47e7395aa58d2
Mario Ernesto Basurto Medrano <mario@d80.mx>
2018-12-17 17:03:58 -0600

    Update index.php

--- index.php
+++ index.php
@@ -228,7 +228,7 @@ setTimeout(function(){
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Nueva Transacci&oacute;n</h4>
+          
         </div>
         <div class="modal-body">
           <form action="?tCodSeccion=<?=$_GET['tCodSeccion']?>" method="post">
@@ -252,7 +252,7 @@ setTimeout(function(){
             </form>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
         </div>
       </div>
       
