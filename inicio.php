6867730ea058abe1ca7fd188a169edf55be97ea9
Mario Ernesto Basurto Medrano <mario@d80.mx>
2018-12-17 11:49:32 -0600

    ...

--- inicio.php
+++ inicio.php
@@ -4,6 +4,23 @@ require_once("cls/cls-sistema.php");
 $clSistema = new clSis();
 session_start();
 
+$bAll = $clSistema->validarPermiso($_GET['tCodSeccion']);
+
+$fhFechaInicio = $_POST['fhFechaConsulta'] ? date('Y-m-d',strtotime($_POST['fhFechaConsulta'])).' 00:00:00' : date('Y-m-d').' 00:00:00';
+$fhFechaTermino = $_POST['fhFechaConsulta'] ? date('Y-m-d',strtotime($_POST['fhFechaConsulta'])).' 23:59:59' : date('Y-m-d').' 23:59:59';
+
+$fhFechaInicio = "'".$fhFechaInicio."'";
+$fhFechaTermino = "'".$fhFechaTermino."'";
+
+$select = "SELECT be.*, cc.tNombres nombreCliente, cc.tApellidos apellidosCliente,
+															su.tNombre as promotor, ce.tNombre Estatus FROM BitEventos be INNER JOIN CatClientes cc ON cc.eCodCliente = be.eCodCliente
+															INNER JOIN CatEstatus ce ON ce.eCodEstatus = be.eCodEstatus
+														LEFT JOIN SisUsuarios su ON su.eCodUsuario = be.eCodUsuario
+                                                        WHERE
+                                                        be.fhFechaEvento between $fhFechaInicio AND $fhFechaTermino".
+												($bAll ? "" : " AND cc.eCodUsuario = ".$_SESSION['sessionAdmin'][0]['eCodUsuario']).
+														" ORDER BY be.fhFechaEvento DESC";
+$rsEventos = mysql_query($select);
 ?>
 
 <div class="row">
@@ -25,54 +42,32 @@ session_start();
                                     </div>
                                     <div class="au-task js-list-load">
                                         <div class="au-task__title">
-                                            <p>[Fecha de Consulta]</p>
+                                            <p><?=date('d/m/Y',strtotime($fhFechaInicio))?></p>
                                         </div>
                                         <div class="au-task-list js-scrollbar3">
-                                            <div class="au-task__item au-task__item--primary">
-                                                <div class="au-task__item-inner">
-                                                    <h5 class="task">
-                                                        <a href="#">[Nombre del cliente]</a>
-                                                    </h5>
-                                                    <span class="time">[Estatus]</span>
-                                                    <span class="time">[Fecha evento] [Hora Montaje]</span>
+                                            <?
+                                                if(mysql_num_rows($rsEventos))
+                                                {
+                                                    while($rEvento = mysql_fetch_array($rsEventos))
+                                                    {
+                                                        ?>
+                                                <div class="au-task__item au-task__item--primary">
+                                                    <div class="au-task__item-inner">
+                                                        <h5 class="task">
+                                                            <a href="#"><?=$rEvento{'nombreCliente'}.' '.$rEvento{'apellidosCliente'}?></a>
+                                                        </h5>
+                                                        <span class="time"><?=$rEvento{'Estatus'}?></span>
+                                                        <span class="time"><?=date('d/m/Y',strtotime($rEvento{'fhFechaEvento'}))?> (<?=date('H:i',strtotime($rEvento{'fhFechaEvento'}))?>)</span>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            <div class="au-task__item au-task__item--primary">
-                                                <div class="au-task__item-inner">
-                                                    <h5 class="task">
-                                                        <a href="#">[Nombre del cliente]</a>
-                                                    </h5>
-                                                    <span class="time">[Estatus]</span>
-                                                    <span class="time">[Fecha evento] [Hora Montaje]</span>
-                                                </div>
-                                            </div>
-                                            <div class="au-task__item au-task__item--primary">
-                                                <div class="au-task__item-inner">
-                                                    <h5 class="task">
-                                                        <a href="#">[Nombre del cliente]</a>
-                                                    </h5>
-                                                    <span class="time">[Estatus]</span>
-                                                    <span class="time">[Fecha evento] [Hora Montaje]</span>
-                                                </div>
-                                            </div>
-                                            <div class="au-task__item au-task__item--primary">
-                                                <div class="au-task__item-inner">
-                                                    <h5 class="task">
-                                                        <a href="#">[Nombre del cliente]</a>
-                                                    </h5>
-                                                    <span class="time">[Estatus]</span>
-                                                    <span class="time">[Fecha evento] [Hora Montaje]</span>
-                                                </div>
-                                            </div>
-                                            <div class="au-task__item au-task__item--primary">
-                                                <div class="au-task__item-inner">
-                                                    <h5 class="task">
-                                                        <a href="#">[Nombre del cliente]</a>
-                                                    </h5>
-                                                    <span class="time">[Estatus]</span>
-                                                    <span class="time">[Fecha evento] [Hora Montaje]</span>
-                                                </div>
-                                            </div>
+                                            <?
+                                                    }
+                                                }
+                                                else
+                                                {
+                                                    echo '<h2>No se han encontrado eventos en la fecha seleccionada</h2>';
+                                                }
+                                            ?>
                                         </div>
                                     </div>
                                 </div>
@@ -93,5 +88,6 @@ function obtenerFecha()
 var fecha = $("#datepicker").datepicker( 'getDate' );
 var fhFecha = new Date(fecha);
 document.getElementById('datepicker1').value = fhFecha.getDate()+'-'+fhFecha.getMonth()+'-'+fhFecha.getFullYear();
+    document.getElementById('Datos').submit();
 }
 </script>
