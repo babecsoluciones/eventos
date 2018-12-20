8151bac0fb3eee09874220a2badeda4754a3ac67
Mario Ernesto Basurto Medrano <mario@d80.mx>
2018-12-20 12:00:44 -0600

    Update cata-tra-con.php

--- cata-tra-con.php
+++ cata-tra-con.php
@@ -27,7 +27,7 @@ session_start();
                                         </thead>
                                         <tbody>
 											<?
-											$select = "SELECT bt.eCodTransaccion, bt.fhFecha, bd.eCodEvento, (cc.tNombres + ' ' + cc.tApellidos) as Cliente, bt.dMonto, ctp.tNombre, (su.tNombre+' '+su.tApellidos) as Usuario FROM BitTransacciones bt INNER JOIN CatTiposPagos ctp ON ctp.eCodTipoPago = bt.eCodTipoPago INNER JOIN BitEventos be ON be.eCodEvento = bt.eCodEvento INNER JOIN CatClientes cc ON cc.eCodCliente = be.eCodCliente INNER JOIN SisUsuarios su ON su.eCodUsuario = bt.eCodUsuario WHERE ORDER BY bt.eCodTransaccion DESC LIMIT 0,25";
+											$select = "SELECT bt.eCodTransaccion, bt.fhFecha, bd.eCodEvento, cc.tNombres as nombreCliente, cc.tApellidos as apellidosCliente, bt.dMonto, ctp.tNombre, (su.tNombre) as nombreUsuario, su.tApellidos as apellidosUsuario FROM BitTransacciones bt INNER JOIN CatTiposPagos ctp ON ctp.eCodTipoPago = bt.eCodTipoPago INNER JOIN BitEventos be ON be.eCodEvento = bt.eCodEvento INNER JOIN CatClientes cc ON cc.eCodCliente = be.eCodCliente INNER JOIN SisUsuarios su ON su.eCodUsuario = bt.eCodUsuario WHERE ORDER BY bt.eCodTransaccion DESC LIMIT 0,25";
 											$rsPublicaciones = mysql_query($select);
 											while($rPublicacion = mysql_fetch_array($rsPublicaciones))
 											{
@@ -35,8 +35,8 @@ session_start();
 											<tr>
                                                 <td><?=sprintf("%07d",$rPublicacion{'eCodTransaccion'})?></td>
                                                 <td><?=date('d/m/Y',strtotime($rPublicacion{'fhFecha'}))?></td>
-												<td><?=utf8_decode($rPublicacion{'Cliente'})?></td>
-                                                <td><?=utf8_decode($rPublicacion{'Usuario'})?></td>
+												<td><?=utf8_decode($rPublicacion{'nombreCliente'} . ' '.$rPublicacion{'apellidosCliente'})?></td>
+                                                <td><?=utf8_decode($rPublicacion{'nombreUsuario'}.' '.$rPublicacion{'apellidosUsuario'})?></td>
 												<td><?=utf8_decode($rPublicacion{'tNombre'})?></td>
                                                 <td class="text-right"> 
 													<button type="button" class="btn btn-secondary mb-1" onclick="window.location='?tCodSeccion=cata-eve-det&eCodEvento=<?=$rPublicacion{'eCodEvento'}?>'">
