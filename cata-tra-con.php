db32a767d2a3226c7a7527df13f83ceb880e7c82
Mario Ernesto Basurto Medrano <mario@d80.mx>
2018-12-20 11:56:26 -0600

    Update cata-tra-con.php

--- cata-tra-con.php
+++ cata-tra-con.php
@@ -20,21 +20,14 @@ session_start();
 												<th>C&oacute;digo</th>
                                                 <th>Fecha</th>
                                                 <th>Cliente</th>
-                                                <th class="text-right">Eventos</th>
+                                                <th class="text-right">Usuario</th>
+                                                <th class="text-right">Forma de Pago</th>
                                                 <th class="text-right"></th>
                                             </tr>
                                         </thead>
                                         <tbody>
 											<?
-											$select = "	SELECT
-														bt.eCodTransaccion,
-														bt.fhFecha,
-														(cc.tNombres + ' ' + cc.tApellidos) as Cliente,
-														(SELECT COUNT(*) FROM BitRegistros WHERE eCodTransaccion = bt.eCodTransaccion) as Compras
-														FROM
-														BitTransacciones bt
-														INNER JOIN CatClientes cc ON cc.eCodCliente = bt.eCodCliente
-														ORDER BY bt.eCodTransaccion DESC LIMIT 0,15";
+											$select = "SELECT bt.eCodTransaccion, bt.fhFecha, bd.eCodEvento, (cc.tNombres + ' ' + cc.tApellidos) as Cliente, bt.dMonto, ctp.tNombre, (su.tNombre+' '+su.tApellidos) as Usuario FROM BitTransacciones bt INNER JOIN CatTiposPagos ctp ON ctp.eCodTipoPago = bt.eCodTipoPago INNER JOIN BitEventos be ON be.eCodEvento = bt.eCodEvento INNER JOIN CatClientes cc ON cc.eCodCliente = be.eCodCliente INNER JOIN SisUsuarios su ON su.eCodUsuario = bt.eCodUsuario WHERE ORDER BY bt.eCodTransaccion DESC LIMIT 0,25";
 											$rsPublicaciones = mysql_query($select);
 											while($rPublicacion = mysql_fetch_array($rsPublicaciones))
 											{
@@ -43,10 +36,11 @@ session_start();
                                                 <td><?=sprintf("%07d",$rPublicacion{'eCodTransaccion'})?></td>
                                                 <td><?=date('d/m/Y',strtotime($rPublicacion{'fhFecha'}))?></td>
 												<td><?=utf8_decode($rPublicacion{'Cliente'})?></td>
-												<td class="text-right"><?=sprintf("%07d",$rPublicacion{'Compras'})?></td>
+                                                <td><?=utf8_decode($rPublicacion{'Usuario'})?></td>
+												<td><?=utf8_decode($rPublicacion{'tNombre'})?></td>
                                                 <td class="text-right"> 
-													<button type="button" class="btn btn-secondary mb-1" data-toggle="modal" data-target="#detTran<?=$rPublicacion{'eCodTransaccion'}?>">
-														Detalles
+													<button type="button" class="btn btn-secondary mb-1" onclick="window.location='?tCodSeccion=cata-eve-det&eCodEvento=<?=$rPublicacion{'eCodEvento'}?>'">
+														Ver Detalles
 													</button>
 												</td>
                                             </tr>
@@ -60,91 +54,3 @@ session_start();
                             
                         </div>
 
-<?
-											$select = "	SELECT
-														bt.eCodTransaccion,
-														bt.fhFecha,
-														(cc.tNombres + ' ' + cc.tApellidos) as Cliente,
-														(SELECT COUNT(*) FROM BitRegistros WHERE eCodTransaccion = bt.eCodTransaccion) as Compras
-														FROM
-														BitTransacciones bt
-														INNER JOIN CatClientes cc ON cc.eCodCliente = bt.eCodCliente
-														ORDER BY bt.eCodTransaccion DESC LIMIT 0,15";
-											$rsPublicaciones = mysql_query($select);
-											while($rPublicacion = mysql_fetch_array($rsPublicaciones))
-											{
-												?>
-<!-- modal medium -->
-			<div class="modal fade" id="mediumModal" tabindex="-1" role="dialog" aria-labelledby="mediumModalLabel" aria-hidden="true">
-				<div class="modal-dialog modal-lg" role="document">
-					<div class="modal-content">
-						<div class="modal-header">
-							<h5 class="modal-title" id="mediumModalLabel">Transaccion #<?=sprintf("%07d",$rPublicacion{'eCodTransaccion'})?></h5>
-							<button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
-								<span aria-hidden="true">&times;</span>
-							</button>
-						</div>
-						<div class="modal-body">
-							<?
-											$select = "	SELECT 
-															cc.*, 
-															ce.tNombre as estatus
-														FROM
-															Catclientes cc
-														INNER JOIN CatEstatus ce ON cc.eCodEstatus = ce.eCodEstatus ORDER BY cc.eCodCliente ASC";
-											$rsPublicaciones = mysql_query($select);
-											$rPublicacion = mysql_fetch_array($rsPublicaciones)
-												?>
-							<p>
-											Cliente: 
-                                                <?=utf8_decode($rPublicacion{'tTitulo'})?>
-												<?=utf8_decode($rPublicacion{'tNombres'})?>
-												<?=utf8_decode($rPublicacion{'tApellidos'})?><br>
-											E-mail: 
-												<?=utf8_decode($rPublicacion{'tCorreo'})?><br>
-											Tel&eacute;fono: 
-												<?=utf8_decode($rPublicacion{'tTelefonoFijo'})?>
-							</p>
-							<table>
-							<thead>
-								<tr>
-									<td><b>Curso / Evento</b></td>
-									<td><b>Monto</b></td>
-									<td><b>Factura</b></td>
-								</tr>
-							</thead>
-								<tbody>
-								<?
-								$select = "	SELECT 
-												br.*,
-												bp.*
-											FROM
-												BitRegistros br
-											INNER JOIN BitPublicaciones bp ON bp.eCodPublicacion=br.eCodPublicacion
-											WHERE
-												br.eCodTransaccion = ".$rPublicacion{'eCodTransaccion'};
-								$rsCarrito = mysql_query($select);
-								while($rCarrito = mysql_fetch_array($rsCarrito))
-								{
-									?>
-									<tr>
-										<td><?=utf8_decode($rCarrito{'tTitulo'})?></td>
-										<td>$<?=number_format($rCarrito{'tTitulo'},2,'.',',')?></td>
-										<td><?=$rCarrito{'bFactura'} ? 'SI' : 'NO' ?></td>
-									</tr>
-									<?
-								}
-								?>
-								</tbody>
-							</table>
-						</div>
-						<div class="modal-footer">
-							<button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-						</div>
-					</div>
-				</div>
-			</div>
-			<!-- end modal medium -->
-<?
-											}
-?>
