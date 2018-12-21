4c9bedb1a1da02afa6e6689515dbf8393794f73a
Mario Ernesto Basurto Medrano <mario@d80.mx>
2018-12-21 13:34:59 -0600

    Update light-eve-det.php

--- light-eve-det.php
+++ light-eve-det.php
@@ -21,102 +21,167 @@ $select = "	SELECT
 $rsClientes = mysql_query($select);
 
 ?>
-<?
-if($_POST)
-{
-    $res = $clSistema -> registrarEvento();
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Detalle del Evento</title>
     
-    if($res)
-    {
-        ?>
-            <div class="alert alert-success" role="alert">
-                El evento se guard&oacute; correctamente!
-            </div>
-<script>
-setTimeout(function(){
-    window.location="?tCodSeccion=cata-eve-con";
-},2500);
-</script>
-<?
+    <style>
+    .invoice-box {
+        max-width: 800px;
+        width:700px;
+        margin: auto;
+        padding: 30px;
+        border: 1px solid #eee;
+        box-shadow: 0 0 10px rgba(0, 0, 0, .15);
+        font-size: 16px;
+        line-height: 24px;
+        font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+        color: #555;
     }
-    else
-    {
-  ?>
-            <div class="alert alert-danger" role="alert">
-                Error al procesar la solicitud!
-            </div>
-<?
+    
+    .invoice-box table {
+        width: 100%;
+        line-height: inherit;
+        text-align: left;
     }
-}
-?>
+    
+    .invoice-box table td {
+        padding: 5px;
+        vertical-align: top;
+    }
+    
+    .invoice-box table tr td:nth-child(2) {
+        text-align: right;
+    }
+    
+    .invoice-box table tr.top table td {
+        padding-bottom: 20px;
+    }
+    
+    .invoice-box table tr.top table td.title {
+        font-size: 45px;
+        line-height: 45px;
+        color: #333;
+    }
+    
+    .invoice-box table tr.information table td {
+        padding-bottom: 40px;
+    }
+    
+    .invoice-box table tr.heading td {
+        background: #eee;
+        border-bottom: 1px solid #ddd;
+        font-weight: bold;
+    }
+    
+    .invoice-box table tr.details td {
+        padding-bottom: 20px;
+    }
+    
+    .invoice-box table tr.item td{
+        border-bottom: 1px solid #eee;
+    }
+    
+    .invoice-box table tr.item.last td {
+        border-bottom: none;
+    }
+    
+    .invoice-box table tr.total td:nth-child(2) {
+        border-top: 2px solid #eee;
+        font-weight: bold;
+    }
+    
+    @media only screen and (max-width: 600px) {
+        .invoice-box table tr.top table td {
+            width: 100%;
+            display: block;
+            text-align: center;
+        }
+        
+        .invoice-box table tr.information table td {
+            width: 100%;
+            display: block;
+            text-align: center;
+        }
+    }
+    
+    /** RTL **/
+    .rtl {
+        direction: rtl;
+        font-family: Tahoma, 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+    }
+    
+    .rtl table {
+        text-align: right;
+    }
+    
+    .rtl table tr td:nth-child(2) {
+        text-align: left;
+    }
+    </style>
+</head>
 
-<link href="dist/easy-autocomplete.min.css" rel="stylesheet" type="text/css">
-<script src="lib/jquery-1.11.2.min.js"></script>
-<script src="dist/jquery.easy-autocomplete.min.js" type="text/javascript" ></script>
-    <center>
-<table style="width:700px">
-    <tr>
-    <td>
-    <div class="col-lg-12">
-    <form id="datos" name="datos" action="<?=$_SERVER['REQUEST_URI']?>" method="post" enctype="multipart/form-data">
-        <input type="hidden" name="eCodEvento" value="<?=$_GET['eCodEvento']?>">
-        <input type="hidden" name="eAccion" id="eAccion">
-                            <div class="col-lg-12">
-								<h2 class="title-1 m-b-25">Detalles del Evento</h2>
-                                <div class="card col-lg-12">
-                                    
-                                    <div class="card-body card-block">
-                                        <!--campos-->
-                                        
-           <div class="form-group">
-              <label>Cliente</label>
-              
-                                                        <?
+<body>
+    <div class="invoice-box">
+        <table cellpadding="0" cellspacing="0">
+            <tr class="top">
+                <td colspan="2">
+                    <table>
+                        <tr>
+                            <td class="title">
+                                <img src="../images/icon/logo.png" style="width:100%; max-width:300px;">
+                            </td>
+                            
+                            <td>
+                                Evento # <?=sprintf("%07d",$_GET['eCodEvento'])?><br>
+                                Fecha: <?=date('d/m/Y',strtotime($rPublicacion{'fhFechaEvento'}))?><br>
+                                Hora de Montaje: <?=date('H:i',strtotime($rPublicacion{'fhFechaEvento'}))?>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            
+            <tr class="information">
+                <td colspan="2">
+                    <table>
+                        <tr>
+                            <td>
+                                 <?
      while($rPaquete = mysql_fetch_array($rsClientes))
 {
          ?>
-                  <?=($rPublicacion{'eCodCliente'}==$rPaquete{'eCodCliente'}) ? $rPaquete{'tNombres'}.' '.$rPaquete{'tApellidos'}.' ('.$rPaquete{'tCorreo'}.')' : ''?>
+                  <?=($rPublicacion{'eCodCliente'}==$rPaquete{'eCodCliente'}) ? $rPaquete{'tNombres'}.' '.$rPaquete{'tApellidos'}.' <br>'.$rPaquete{'tCorreo'}.'<br>'$rPaquete{'tTelefono'} : ''?>
                   <?
 }
     ?>
-      
-              
-               
-           </div>
-           <div class="form-group">
-              <label>Fecha del Evento</label>
-              <?=date('d/m/Y',strtotime($rPublicacion{'fhFechaEvento'}))?>
-           </div>
-           <div class="form-group">
-              <label>Hora de Montaje</label>
-              <?=date('H:i',strtotime($rPublicacion{'fhFechaEvento'}))?>
-           </div>
-           <div class="form-group">
-              <label>Direcci&oacute;n</label>
-              <?=nl2br(base64_decode(utf8_decode($rPublicacion{'tDireccion'})))?>
-           </div>
-           <div class="form-group">
-              <label>Observaciones</label>
-              <?=nl2br(base64_decode(utf8_decode($rPublicacion{'tObservaciones'})))?>
-           </div>
-           
-                                        <!--campos-->
-                                    </div>
-                                </div>
-                                <div class="col-lg-12">
-                                
-                                    <div class="card-body card-block">
-                                    <table class="table table-borderless table-striped table-earning" id="table">
-                                        <thead>
-                                            
-                                            <tr>
-                                                <th></th>
-												<th>Paquete</th>
-                                                <th>Cantidad</th>
-                                                <th>Precio</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
+                            </td>
+                            
+                            <td>
+                                <?=nl2br(base64_decode(utf8_decode($rPublicacion{'tDireccion'})))?>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            
+            
+            
+            <tr class="heading">
+                <td>
+                    Descripci&oacute;n
+                </td>
+                
+                <td>
+                    Precio
+                </td>
+            </tr>
+            
+            
+												?>
+            
 											<?
                                             $i = 0;
 											$select = "	SELECT DISTINCT
@@ -128,25 +193,22 @@ setTimeout(function(){
                                                         INNER JOIN RelEventosPaquetes rep ON rep.eCodServicio = cs.eCodServicio and rep.eCodTipo = 1
                                                         WHERE rep.eCodEvento = ".$_GET['eCodEvento'];
 											$rsPublicaciones = mysql_query($select);
-                                            
+                                            $dTotalEvento = 0;
 											while($rPublicacion = mysql_fetch_array($rsPublicaciones))
 											{
 												?>
-											<tr id="paq<?=$i?>">
-                                                <td></td>
-                                                <td>
-                                                    <input type="hidden" name="eCodServicio<?=$i?>" id="eCodServicio<?=$i?>" value="<?=$rPublicacion{'eCodServicio'}?>">
-                                                    <input type="hidden" name="eCantidad<?=$i?>" id="eCantidad<?=$i?>" value="<?=$rPublicacion{'eCantidad'}?>">
-                                                    <input type="hidden" name="eCodTipo<?=$i?>" id="eCodTipo<?=$i?>" value="<?=$rPublicacion{'eCodTipo'}?>">
-                                                    <?=$rPublicacion{'tNombre'}?>
-                                                </td>
-                                                <td>
-                                                    <?=$rPublicacion{'eCantidad'}?>
-                                                </td>
-												<td>$<?=number_format($rPublicacion{'dPrecioVenta'}*$rPublicacion{'eCantidad'},2)?><input type="hidden" id="totalServ<?=$i?>" value="<?=($rPublicacion{'dPrecioVenta'}*$rPublicacion{'eCantidad'})?>"></td>
-                                            </tr>
+											<tr class="item">
+                <td>
+                    <?=utf8_decode($rPublicacion{'tNombre'})?>
+                </td>
+                
+                <td>
+                    $<?=number_format($rPublicacion{'dPrecioVenta'}*$rPublicacion{'eCantidad'},2)?>
+                </td>
+            </tr>
 											<?
 											$i++;
+                                                $dTotalEvento = $dTotalEvento + ($rPublicacion{'dPrecioVenta'}*$rPublicacion{'eCantidad'});
 											}
                                             $select = "	SELECT DISTINCT
 															cs.tNombre,
@@ -161,65 +223,32 @@ setTimeout(function(){
 											while($rPublicacion = mysql_fetch_array($rsPublicaciones))
 											{
 												?>
-											<tr id="paq<?=$i?>">
-                                                <td></td>
-                                                <td>
-                                                    <input type="hidden" name="eCodServicio<?=$i?>" id="eCodServicio<?=$i?>" value="<?=$rPublicacion{'eCodServicio'}?>">
-                                                    <input type="hidden" name="eCantidad<?=$i?>" id="eCantidad<?=$i?>" value="<?=$rPublicacion{'eCantidad'}?>">
-                                                    <input type="hidden" name="eCodTipo<?=$i?>" id="eCodTipo<?=$i?>" value="<?=$rPublicacion{'eCodTipo'}?>">
-                                                    <?=$rPublicacion{'tNombre'}?>
-                                                </td>
-                                                <td>
-                                                    <?=$rPublicacion{'eCantidad'}?>
-                                                </td>
-												<td>$<?=number_format($rPublicacion{'dPrecioVenta'}*$rPublicacion{'eCantidad'},2)?><input type="hidden" id="totalServ<?=$i?>" value="<?=($rPublicacion{'dPrecioVenta'}*$rPublicacion{'eCantidad'})?>"></td>
-                                            </tr>
+											<tr class="item">
+                <td>
+                    <?=utf8_decode($rPublicacion{'tNombre'})?>
+                </td>
+                
+                <td>
+                    $<?=number_format($rPublicacion{'dPrecioVenta'}*$rPublicacion{'eCantidad'},2)?>
+                </td>
+            </tr>
 											<?
 											$i++;
+                                                $dTotalEvento = $dTotalEvento + ($rPublicacion{'dPrecioVenta'}*$rPublicacion{'eCantidad'});
 											}
 											?>
-											<tr>
-												<td> <input type="hidden" id="totEvento" value="0"></td>
-											<td colspan="3" align="right" id="totalVenta"></td>
-											</tr>
-                                        </tbody>
-                                    </table>
-      
-                                    </div>
-                                </div>
-								
-								
-                                
-                            </div>
-    </form>
+            
+            
+            
+            <tr class="total">
+                <td></td>
+                
+                <td>
+                   Total: $$<?=number_format($dTotalEvento,2)?>
+                </td>
+            </tr>
+        </table>
     </div>
-                        
-        </td></tr></table>
-        </center>
-<script> 
+</body>
+</html>
 
-    function calcular()
-    {
-        var venta = 0, abono = 0;
-        var cmbTotal = document.querySelectorAll("[id^=totalServ]");
-        cmbTotal.forEach(function(nodo){
-            
-            venta = parseInt(venta) + parseInt(nodo.value);
-            
-        });
-		
-		var cmbAbono = document.querySelectorAll("[id^=abono]");
-        cmbAbono.forEach(function(nodo){
-            
-            abono = parseInt(abono) + parseInt(nodo.value);
-            
-        });
-        
-        document.getElementById('totalVenta').innerHTML = "Total: $"+venta.toFixed(2);
-		document.getElementById('totAbono').innerHTML = "$"+abono.toFixed(2);
-		document.getElementById('totRestante').innerHTML = "$"+(venta-abono);
-    }
-	
-	calcular();
-
-		</script>
